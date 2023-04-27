9e46bc5202d7fb80df218510542a0a044c566469
Henrique Dionisio Ferreira <henriquedionisiof@brq.com>
2023-04-27 06:22:39 -0300

    novas modificações - clean code

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,19 +1,24 @@
 
-palavra_secreta = "banana".lower()
-letras_certas = ["_", "_", "_", "_", "_", "_"]
-erros = 0
-acertou = False
-enforcou = False
+import random
+
 
 def jogar_forca():
+
+    # inicia o jogo da forca
+    arquivo = digitar_palavras()
+    palavra_secreta = gerar_palavra_secreta(arquivo)
+
+    letras_certas = ["_" for letra in palavra_secreta]
+    erros = 0
+    pontos = 0
+    opcao = True
 
     print("* " * 10)
     print("Bem vindo ao jogo da forca!")
     print("* " * 10)
 
-
     # início dos palpites
-    while not enforcou and not acertou:
+    while opcao:
         
         chute = input("Qual a letra?").lower().strip()
 
@@ -26,24 +31,47 @@ def jogar_forca():
                     print(f"Letra {chute} encontrada no índice: {index}")
                     letras_certas.__setitem__(index, chute)
                     print(letras_certas)
+
                 index += 1
         else:
             erros += 1
-        
-        # verificação de enforcamento
-        enforcou = erros is 6
 
-        # verificação de acerto da palavra
-        acertou = "_" not in letras_certas
-
-
-    if acertou:
+    if "_" not in letras_certas:
         print(f"Você ganhou!! :D\nQtd de erros: {erros}")
+        pontos += 1
+
     else:
         print("Você cometeu 6 erros!\nPerdeu! :'(")
+        pontos -= 1
 
-    print("Fim do jogo!")
+    opcao = input("Jogar novamente? Digite qualquer tecla ou enter para sair ")
 
+print("Fim do jogo!")
+
+
+def digitar_palavras():
+    
+    # insere uma qtd de palavras determinada no arquivo para o jogo da forca
+
+    with open("palavras.txt", "w") as arquivo:
+        
+        qtd_palavras = int(input("Digite a qtd de palavras para a forca: "))
+        for i in range(0, qtd_palavras):
+            arquivo.write(input("Digite uma palavra"))
+            arquivo.write("\n")
+
+    return arquivo
+
+def gerar_palavra_secreta(arquivo: str) -> str:
+    # recebe um arquivo com palavras e retorna uma palavra selecionada aleatoriamente do arquivo
+    
+    palavra_secreta = ""
+    with open("palavras.txt", "r") as arquivo:
+
+        palavras = [ linha for linha in arquivo ]
+        palavra_secreta = palavras.__getitem__(random.randint(0, len(palavras)-1)).strip().lower()
+
+    return palavra_secreta
 
 if __name__ == "__main__":
     jogar_forca()
