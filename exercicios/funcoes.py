6a33b4c298d746f96d626dc3176dd98f583707ee
dionisiohenriq <henriquedionisio.dionisioferre@gmail.com>
2023-07-25 03:14:15 -0300

    Teste

---
+++ exercicios/funcoes.py
@@ -0,0 +1,13 @@
+def multiplicador(multiplicador):
+    def numero(numero):
+        return multiplicador * numero
+
+    return numero
+
+
+num = int(input("Insira um número:"))
+multiplicar = int(input("Insira um multiplicador:"))
+
+
+duplicar = multiplicador(multiplicar)
+print(duplicar(num))
