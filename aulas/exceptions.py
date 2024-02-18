b970875f68bea20866c45aa46f2e2f5af8c499e7
Dionisiohenriq <henriquedionisio.dionisioferre@gmail.com>
2024-02-18 00:36:16 -0300

    novos exercícios - hackerhank

--- aulas/exceptions.py
+++ aulas/exceptions.py
@@ -1,39 +1,87 @@
-# try:
-#     a = 18
-#     b = 0    
-#     c = a / b
-# except ZeroDivisionError as e:
-#     print(f'Error when dividing by zero {e}')
-# except NameError as e:
-#     print(f'Erro, variable not found {e}')
-# except TypeError as e:
-#     print(f'TypeError {e}')
-# print('Fim')
+try:
+    a = 18
+    b = 0    
+    c = a / b
+except ZeroDivisionError as e:
+    print(f'Error when dividing by zero {e}')
+except NameError as e:
+    print(f'Erro, variable not found {e}')
+except (TypeError, IndexError) as e:
+    print(f'TypeError {e}')
+except Exception as e:
+    print(f'Error {e}')
+print('Fim')
 
 
-def miniMaxSum(arr: []) -> str:
+# def miniMaxSum(arr: []) -> str:
+#     # Write your code here
+#     arr2 = arr.copy()
+    
+#     arr.remove(max(arr))
+#     arr2.remove(min(arr))
+    
+#     max_sum = sum(arr)
+#     min_sum = (sum(arr2))
+
+#     print(f'{max_sum} {min_sum}')
+    
+
+# if __name__ == '__main__':
+
+#     arr = [1, 2, 3, 4, 5]
+
+#     miniMaxSum(arr)
+
+#!/bin/python3
+
+# import math
+# import os
+# import random
+# import re
+# import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+# def gradingStudents(grades):
     # Write your code here
-    max_number = max(arr)
-    min_number = min(arr)
+    # count_difference = 0
+    # index = 0
+    # grade_aux = 0
     
-    max_number_index = arr.index(max_number)
-    min_number_index = arr.index(min_number)
+    # print(grades)
     
-    print(max_number_index, min_number_index)
+    # for grade in grades:
+        
+    #     print(grade)
 
-    print()
+    #     grade_aux = grade
+        
+    #     if grade > 37:
+                
+    #         while grade_aux % 5 != 0:
+    #             count_difference += 1
+    #             grade_aux += 1
+            
+    #         if count_difference < 3:
+    #             print(grade, grade_aux)
+    #             grade = grade_aux
+    #             grades[index] = grade
+        
+    #     index += 1
+    #     count_difference = 0
+        
+    # return grades
+#     return map(lambda grade: round(grade * 2, -1) // 2 if grade >= 38 and grade % 5 >= 3 else grade, grades)
 
-    arr_less_min = arr.pop(min_number_index)
-    arr_less_max = arr.pop(max_number_index)
-    
-    max_sum = sum(arr_less_max)
-    min_sum = sum(arr_less_min)
-    
-    print(f'{max_sum} {min_sum}')
-    
-    
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    arr = [1, 2, 3, 4, 5]
+#     grades = [73, 67, 38, 33]
 
-    miniMaxSum(arr)
+#     result = gradingStudents(grades)
+
+#     print(result)
