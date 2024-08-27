b2260d55fde2eb28af23d9f47e43b2f7b0ff9609
Dionisiohenrique <henriquedionisio.dionisioferre@gmail.com>
2024-08-27 02:02:24 -0300

    django new api

---
+++ projects/django_api/poke_api/api/models.py
@@ -0,0 +1,24 @@
+# Create your models here.
+import datetime
+from django.db import models
+from django.utils import timezone
+
+
+class Pokemon(models.Model):
+    pokemon_name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self) -> str:
+        return self.pokemon_name
+
+
+class Trainers(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    trainer_name = models.CharField(max_length=200)
+    creation_date = models.DateTimeField('date_created')
+
+    def __str__(self) -> str:
+        return self.trainer_name
+
+    def was_published_recently(self):
+        return self.creation_date >= timezone.now() - datetime.timedelta(days=1)
